8f2d5cb2ca5b40f74b465df5934484a44992a3e0
PrateekChitre <136295348+PrateekChitre@users.noreply.github.com>
2023-11-19 22:33:36 +0530

    Update rsa.c

--- rsa.c
+++ rsa.c
@@ -1,83 +1,122 @@
-#include <stdio.h>
-#include <string.h>
-
-// Function for the Key Scheduling Algorithm (KSA)
-void KSA(unsigned char S[], unsigned char key[], int key_len, int N) {
-    int i, j;
-    j = 0;
-    for (i = 0; i < N; i++) {
-        j = (j + S[i] + key[i % key_len]) % N;
-        // Swap S[i] and S[j]
-        unsigned char temp = S[i];
-        S[i] = S[j];
-        S[j] = temp;
-    }
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<math.h> 
+#include<string.h>
+ 
+long int p,q,n,t,flag,e[100],d[100],temp[100],j,m[100],en[100],i;
+char msg[100];
+int prime(long int);
+void ce();
+long int cd(long int);
+void encrypt();
+void decrypt();
+void main() {
+	printf("\nENTER FIRST PRIME NUMBER\n");
+	scanf("%d",&p);
+	flag=prime(p);
+	if(flag==0) {
+		printf("\nWRONG INPUT\n");
+		getch();
+		exit(1);
+	}
+	printf("\nENTER ANOTHER PRIME NUMBER\n");
+	scanf("%d",&q);
+	flag=prime(q);
+	if(flag==0||p==q) {
+		printf("\nWRONG INPUT\n");
+		getch();
+		exit(1);
+	}
+	printf("\nENTER MESSAGE\n");
+	fflush(stdin);
+	scanf("%s",msg);
+	for (i=0;msg[i]!=NULL;i++)
+		m[i]=msg[i];
+	n=p*q;
+	t=(p-1)*(q-1);
+	ce();
+	printf("\nPOSSIBLE VALUES OF e AND d ARE\n");
+	for (i=0;i<j-1;i++)
+	    printf("\n%ld\t%ld",e[i],d[i]);
+	encrypt();
+	decrypt();
+	getch();
 }
-
-// Function for the Pseudo-Random Generation Algorithm (PGRA)
-void PGRA(unsigned char S[], unsigned char data[], int data_len, int N) {
-    int i, j;
-    i = j = 0;
-    for (int k = 0; k < data_len; k++) {
-        i = (i + 1) % N;
-        j = (j + S[i]) % N;
-        // Swap S[i] and S[j]
-        unsigned char temp = S[i];
-        S[i] = S[j];
-        S[j] = temp;
-        int t = (S[i] + S[j]) % N;
-        data[k] ^= S[t]; // XOR operation with the state vector
-    }
+int prime(long int pr) {
+	int i;
+	j=sqrt(pr);
+	for (i=2;i<=j;i++) {
+		if(pr%i==0)
+		    return 0;
+	}
+	return 1;
 }
-
-int main() {
-    unsigned char S[256]; // State vector for RC4
-    unsigned char key[] = "SecretKey"; // Encryption key
-    unsigned char input_text[] = "Hello, RC4!"; // Input text
-    unsigned char ciphertext[256]; // Encrypted data (ciphertext)
-    unsigned char decrypted_data[256]; // Decrypted data
-
-    int N = 256; // Number of elements in the state vector
-
-    int key_len = strlen(key);
-    int text_len = strlen(input_text);
-
-    printf("Input text: %s\n", input_text);
-    printf("Key: %s\n\n", key);
-
-    // Initialize S
-    for (int i = 0; i < N; i++) {
-        S[i] = i;
-    }
-
-    // Perform the Key Scheduling Algorithm (KSA) for both encryption and decryption
-    KSA(S, key, key_len, N);
-
-    // Perform encryption using PGRA
-    PGRA(S, input_text, text_len, N);
-
-    printf("Encryption complete.\n");
-
-    // Display ciphertext
-    printf("Ciphertext: ");
-    for (int i = 0; i < text_len; i++) {
-        printf("%02X ", input_text[i]);
-    }
-    printf("\n\n");
-
-    // Reset S
-    for (int i = 0; i < N; i++) {
-        S[i] = i;
-    }
-
-    // Perform the Key Scheduling Algorithm (KSA) for decryption
-    KSA(S, key, key_len, N);
-
-    // Perform decryption using PGRA
-    PGRA(S, input_text, text_len, N);
-
-    // Display decrypted data
-    printf("Decrypted data: %s\n", input_text);
-
-    return 0;
+void ce() {
+	int k;
+	k=0;
+	for (i=2;i<t;i++) {
+		if(t%i==0)
+		    continue;
+		flag=prime(i);
+		if(flag==1&&i!=p&&i!=q) {
+			e[k]=i;
+			flag=cd(e[k]);
+			if(flag>0) {
+				d[k]=flag;
+				k++;
+			}
+			if(k==99)
+			        break;
+		}
+	}
+}
+long int cd(long int x) {
+	long int k=1;
+	while(1) {
+		k=k+t;
+		if(k%x==0)
+		    return(k/x);
+	}
+}
+void encrypt() {
+	long int pt,ct,key=e[0],k,len;
+	i=0;
+	len=strlen(msg);
+	while(i!=len) {
+		pt=m[i];
+		pt=pt-96;
+		k=1;
+		for (j=0;j<key;j++) {
+			k=k*pt;
+			k=k%n;
+		}
+		temp[i]=k;
+		ct=k+96;
+		en[i]=ct;
+		i++;
+	}
+	en[i]=-1;
+	printf("\nTHE ENCRYPTED MESSAGE IS\n");
+	for (i=0;en[i]!=-1;i++)
+	    printf("%c",en[i]);
+}
+void decrypt() {
+	long int pt,ct,key=d[0],k;
+	i=0;
+	while(en[i]!=-1) {
+		ct=temp[i];
+		k=1;
+		for (j=0;j<key;j++) {
+			k=k*ct;
+			k=k%n;
+		}
+		pt=k+96;
+		m[i]=pt;
+		i++;
+	}
+	m[i]=-1;
+	printf("\nTHE DECRYPTED MESSAGE IS\n");
+	for (i=0;m[i]!=-1;i++)
+	printf("%c",m[i]); 
 }
